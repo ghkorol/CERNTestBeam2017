adf58ada421433a56fccc63f56372a2d251c09a5
ghkorol <jeka89.23@gmail.com>
2017-10-12 18:13:02 +0200

    Make the path for common use case

--- run/run.sh
+++ run/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mkdir /home/korol/WC/run/$1
+mkdir $1
 if [ ! -e $1.list ]
 then
-  #ls /home/korol/WC/data/$1 | grep \.bin | grep $2 > $1.list
-  ls /home/korol/WC/data/$1 | grep \.bin > ./$1/$1.list
+  #ls ./../data/$1 | grep \.bin | grep $2 > $1.list
+  ls ./../data/$1 | grep \.bin > ./$1/$1.list
 fi
 
-/home/korol/WC/read ./$1/$1.list /home/korol/WC/data/$1/ ./$1/$1.root
+./../read ./$1/$1.list ./../data/$1/ ./$1/$1.root
 #HEAPCHECK=normal LD_PRELOAD=/usr/lib/libtcmalloc.so /home/korol/WC/read ./$1/$1.list /home/korol/WC/data/$1/ ./$1/$1.root
